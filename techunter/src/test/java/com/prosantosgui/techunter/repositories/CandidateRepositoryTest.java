56bbcfbfd441f5c38ea57b79ccb2c88d9229ab71
Guilherme dos Santos <prfsantosgui546@gmail.com>
2023-12-27 13:30:46 -0300

    CandidateRepository BDD tests

--- techunter/src/test/java/com/prosantosgui/techunter/repositories/CandidateRepositoryTest.java
+++ techunter/src/test/java/com/prosantosgui/techunter/repositories/CandidateRepositoryTest.java
@@ -2,11 +2,13 @@ package com.prosantosgui.techunter.repositories;
 
 import com.prosantosgui.techunter.model.Candidate;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaSystemException;
 
 import java.util.Optional;
 
@@ -17,91 +19,90 @@ class CandidateRepositoryTest {
 
     @Autowired
     private CandidateRepository candidateRepository;
+    private Candidate candidate0;
+
+    @BeforeEach
+    public void setup(){
+        candidate0 = new Candidate("Vino23",
+                "21451",
+                "Vinicius",
+                "2154156165",
+                "Rio de Janeiro",
+                true,
+                "RJ");
+    }
 
     @Test
-    @DisplayName("Persist Candidate in database when successful")
-    void save_PersistCandidate_WhenSuccessful(){
-        Candidate candidateToBeSaved = new Candidate("Vino23", "21451", "Vinicius", "2154156165", "Rio de Janeiro", true, "RJ");
-        Candidate candidateSaved = this.candidateRepository.save(candidateToBeSaved);
+    @DisplayName("Given Candidate When Save Then return saved Candidate")
+    void givenCandidateWhenSaveThenReturnSavedCandidate() {
+        Candidate candidate = candidateRepository.save(candidate0);
 
-        Assertions.assertThat(candidateSaved).isNotNull();
-        Assertions.assertThat(candidateSaved.getLogin()).isNotNull();
-        Assertions.assertThat(candidateToBeSaved.getLogin()).isEqualTo(candidateSaved.getLogin());
+        Assertions.assertThat(candidate).isEqualTo(candidate0);
+        Assertions.assertThat(candidate).isNotNull();
+        Assertions.assertThat(candidate.getLogin()).isNotEmpty();
+
+    }
+    @Test
+    @DisplayName("Given Candidate with not-null field value When Save Should throw DataIntegrityViolationException")
+    void givenCandidateWithNotNullFieldValueWhenSaveShouldThrowDataIntegrityViolationException(){
+        candidate0.setName(null);
+
+        Assertions.assertThatExceptionOfType(DataIntegrityViolationException.class).isThrownBy(() -> {
+            candidateRepository.save(candidate0);
+        }).withMessageContaining("not-null property references a null or transient value");
 
     }
 
-    @Test()
-    @DisplayName("Persist Candidate with null mandatory fields")
-    void save_PersistCandidate_WhenNotSuccessful(){
-        Candidate candidateToBeSaved = new Candidate(null, null, null, "2154156165", "Rio de Janeiro", true, "RJ");
-        try {
-           Candidate candidateSaved = this.candidateRepository.save(candidateToBeSaved);
-           Assertions.assertThat(candidateSaved).isNull();
-        }catch (Exception e){
-            Assertions.assertThat(e.getMessage()).containsAnyOf("not-null property references a null or transient value", "ids for this class must be manually assigned before calling save()");
-        }
+    @Test
+    @DisplayName("Given Candidate with Id field null When Save should throw JpaSystemException")
+    void givenCandidateWithIdFieldNullWhenSaveShouldThrowJpaSystemException() {
+        candidate0.setLogin(null);
+
+        Assertions.assertThatExceptionOfType(JpaSystemException.class).isThrownBy(()->{
+            candidateRepository.save(candidate0);
+        }).withMessageContaining("ids for this class must be manually assigned before calling save()");
 
     }
 
-
     @Test
-    @DisplayName("Save updated Candidate in database when successful")
-    void save_UpdateCandidate_WhenSuccessful(){
-        Candidate candidateToBeSaved = new Candidate("Vino23", "21451", "Vinicius", "2154156165", "Rio de Janeiro", true, "RJ");
-        Candidate candidateSaved = this.candidateRepository.save(candidateToBeSaved);
+    @DisplayName("Given Candidate When update fields Successfully")
+    void givenCandidateWhenUpdateFieldsSuccessfully() {
+        Candidate candidateSaved = this.candidateRepository.save(candidate0);
+        String newName = "Viniccius";
+        String newLocation = "São Paulo";
 
-        candidateSaved.setName("Viniccius");
-
+        candidateSaved.setName(newName);
+        candidateSaved.setLocation(newLocation);
         Candidate updatedCandidate = this.candidateRepository.save(candidateSaved);
 
         Assertions.assertThat(updatedCandidate).isNotNull();
         Assertions.assertThat(updatedCandidate.getName()).isNotNull();
-        Assertions.assertThat(updatedCandidate.getName()).isEqualTo(candidateSaved.getName());
-
+        Assertions.assertThat(updatedCandidate.getLocation()).isNotNull();
+        Assertions.assertThat(updatedCandidate.getName()).isEqualTo(newName);
+        Assertions.assertThat(updatedCandidate.getLocation()).isEqualTo(newLocation);
     }
 
     @Test
-    @DisplayName("Save updated Candidate in database when not successful")
-    void save_UpdateCandidate_WhenNotSuccessful(){
-        Candidate candidateToBeSaved = new Candidate("Vino23", "21451", "Vinicius", "2154156165", "Rio de Janeiro", true, "RJ");
-        Candidate candidateSaved = this.candidateRepository.save(candidateToBeSaved);
-
-        candidateSaved.setLogin(null);
-        candidateSaved.setName(null);
-        candidateSaved.setPassword(null);
-        try {
-            Candidate updatedCandidate = this.candidateRepository.save(candidateSaved);
-        }catch (Exception e){
-            Assertions.assertThat(e.getMessage()).isNotNull();
-        }
-
-    }
-
-    @Test
-    @DisplayName("Delete candidate in database when successful")
-    void delete_RemoveCandidate_WhenSuccessful(){
-
-        Candidate candidateToBeSaved = new Candidate("Vino23", "21451", "Vinicius", "2154156165", "Rio de Janeiro", true, "RJ");
-        Candidate candidateSaved = this.candidateRepository.save(candidateToBeSaved);
+    @DisplayName("Given Candidate When Delete successful")
+    void givenCandidateWhenDeleteSuccessful() {
+        Candidate candidateSaved = candidateRepository.save(candidate0);
 
         this.candidateRepository.delete(candidateSaved);
-
-        Optional<Candidate> id = this.candidateRepository.findById(candidateSaved.getLogin());
+        Optional<Candidate> id = candidateRepository.findById(candidateSaved.getLogin());
 
         Assertions.assertThat(id).isEmpty();
     }
 
     @Test
-    @DisplayName("Delete candidate in database when not successful")
-    void delete_RemoveCandidate_WhenNotSuccessful(){
+    @DisplayName("Given Candidate When Delete not successful")
+    void givenCandidateWhenDeleteNotSuccessful(){
 
-        Candidate candidateToBeSaved = new Candidate("Vino23", "21451", "Vinicius", "2154156165", "Rio de Janeiro", true, "RJ");
-        Candidate candidateSaved = this.candidateRepository.save(candidateToBeSaved);
+        Candidate candidateSaved = candidateRepository.save(candidate0);
 
         this.candidateRepository.deleteById("Vino22");
-
-        Optional<Candidate> id = this.candidateRepository.findById(candidateSaved.getLogin());
+        Optional<Candidate> id = candidateRepository.findById(candidateSaved.getLogin());
 
         Assertions.assertThat(id).isPresent();
     }
+
 }
